a591fb5dd6f29d55675ebd60aa7a16848ff5c013
Md-Mansor <mansor.me777@ gmail.com>
2023-12-22 02:07:08 +0600

    initial setup

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+
+
+const NavBar = () => {
+    return (
+        <div className=" flex justify-center gap-52">
+            <div>Home</div>
+            <div>About</div>
+            <div>Contact</div>
+        </div>
+    );
+};
+
+export default NavBar;
